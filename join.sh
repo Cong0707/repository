191c008c95b2a3ca53e8c3f64ed01b403774cdc8
cong <49234756+Cong0707@users.noreply.github.com>
2024-02-13 20:36:07 +0800

    Update join.sh

--- join.sh
+++ join.sh
@@ -1,1 +1,1 @@
-curl -sfL https://rancher-mirror.rancher.cn/k3s/k3s-install.sh | INSTALL_K3S_MIRROR=cn K3S_URL=https://new.xem8k5.top:6443 K3S_TOKEN="K10f4b16d9479cae4c5c7b816df26634034f437c911ebf7603551834d56cfa5e343::server:ce7df23dfa711b2918ddbc047a7b9c50" sh -
+curl -sfL https://rancher-mirror.rancher.cn/k3s/k3s-install.sh | INSTALL_K3S_MIRROR=cn K3S_URL=https://new.xem8k5.top:6443 K3S_TOKEN="K10c3088477a5d7a16321188c718ea14296fe8d93f430dbed8aa87c64cd5f51af86::server:bc54ddf7e33671e60e7b7a607a8bf6cd" sh -
