64b21139bb0c62fb1b91838daa4e47f5833da999
cong <49234756+Cong0707@users.noreply.github.com>
2024-02-13 20:06:30 +0800

    Create join.sh

---
+++ join.sh
@@ -0,0 +1,1 @@
+curl -sfL https://rancher-mirror.rancher.cn/k3s/k3s-install.sh | INSTALL_K3S_MIRROR=cn K3S_URL=https://new.xem8k5.top:6443 K3S_TOKEN="K10f4b16d9479cae4c5c7b816df26634034f437c911ebf7603551834d56cfa5e343::server:ce7df23dfa711b2918ddbc047a7b9c50" sh -
