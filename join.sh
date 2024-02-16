a1b48e5baa513081226fc852fc253c364a27a9cf
cong <49234756+Cong0707@users.noreply.github.com>
2024-02-16 14:37:19 +0800

    Update join.sh

--- join.sh
+++ join.sh
@@ -1,3 +1,3 @@
 sudo curl -s https://install.zerotier.com/ | sudo bash
 sudo zerotier-cli join 6ab565387a0d0396
-sudo curl -sfL https://rancher-mirror.rancher.cn/k3s/k3s-install.sh | INSTALL_K3S_MIRROR=cn K3S_URL=https://172.30.25.108:6443 K3S_TOKEN="K100ffe807e81c499b22a61ca54d7abe473a0d00e0e5dc7bc932bf59de175ad1ed4::server:1869897287675e5eadeaba7ab627e7f2" sh -
+sudo curl -sfL https://rancher-mirror.rancher.cn/k3s/k3s-install.sh | INSTALL_K3S_MIRROR=cn K3S_URL=https://172.30.25.108:6443 K3S_TOKEN="K10641a92a44256ef9dd52cf711091c1ef9a0ac392c49107cfdcb1d541a4e2dd76f::server:315d5beb2391440a5acd97e2f78751f8" sh -
